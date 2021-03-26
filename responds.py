738b3449f211bf8338d62cfb1cce2cab1ea94ef8
Nick Werblun <nickwerblun@gmail.com>
2021-03-25 21:57:37 -0700

    added newline at end

--- responds.py
+++ responds.py
@@ -7,11 +7,11 @@ class Responds(ABC):
 	@abstractmethod
 	async def get_response(message: discord.Message) -> discord.Embed:
 		pass
-	
+		
 	@abstractmethod
 	async def get_help_message(message: discord.Message) -> discord.Embed:
 		pass
-	
+
 	@abstractmethod
 	async def get_usage_message(message: discord.Message) -> discord.Embed:
 		pass
