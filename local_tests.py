6172dcbb5e372242be2fd6fc8afc81713d5adbe5
nwerblun <nwerblun@users.noreply.github.com>
2021-03-30 19:04:58 -0700

    Delete local_tests.py

--- local_tests.py
+++
@@ -1,9 +0,0 @@
-from battlemap import Battlemap
-import asyncio
-
-p = Battlemap(3, 5)
-print("hi")
-print(asyncio.run(p.add_wall((0, 1), (2, 1))))
-print(asyncio.run(p.add_door((1, 1))))
-print(asyncio.run(p.find_path((0,0), (0,4))))
-print("done")
